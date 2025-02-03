84be7464ad33e5e65c282aaebfd21e8022b249c1
Cedric <25cedliu@student.cfschools.org>
2025-02-03 11:37:55 -0600

    added token

--- operationmonsterisland/Main.gd
+++ operationmonsterisland/Main.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@onready var hoku : Sprite2D = $Hoku
+@onready var hokuToken : Sprite2D = $HokuToken
 @onready var spotTwo: Marker2D = $SpotTwo
 
 
@@ -8,13 +8,13 @@ func _unhandled_input(event: InputEvent) -> void:
 	var tween = create_tween()
 	
 	if Input.is_action_just_pressed("ui_left"):
-		tween.tween_property(hoku, "position", Vector2(hoku.position.x - 10, hoku.position.y), 0.1)
+		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x - 10, hokuToken.position.y), 0.1)
 		
 	if Input.is_action_just_pressed("ui_right"):
-		tween.tween_property(hoku, "position", Vector2(hoku.position.x + 10, hoku.position.y), 0.1)
+		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x + 10, hokuToken.position.y), 0.1)
 	
 	if Input.is_action_just_pressed("ui_up"):
-		tween.tween_property(hoku, "position", Vector2(hoku.position.x, hoku.position.y - 10), 0.1)
+		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y - 10), 0.1)
 	
 	if Input.is_action_just_pressed("ui_down"):
-		tween.tween_property(hoku, "position", Vector2(hoku.position.x, hoku.position.y + 10), 0.1)
+		tween.tween_property(hokuToken, "position", Vector2(hokuToken.position.x, hokuToken.position.y + 10), 0.1)
